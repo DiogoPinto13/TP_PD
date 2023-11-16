e877a6d13685375ccd90a8d8603b19ba89ede36b
DiogoPinto13 <pinto.diogo2002@gmail.com>
2023-11-16 22:21:11 +0000

    Changes by cardo

--- src/User/UIControllers/Eventos.java
+++ src/User/UIControllers/Eventos.java
@@ -1,4 +1,26 @@
 package User.UIControllers;
 
 public class Eventos {
+    int id;
+    String designcao, local, horaInicio, horaFim;
+
+    public void setID(int id) {
+        this.id=id;
+    }
+
+    public void setDesignacao(String designacao) {
+        this.designcao=designacao;
+    }
+
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio=horaInicio;
+    }
+
+    public void setHoraFim(String h) {
+        horaFim=h;
+    }
+
+    public void setPlace(String place){
+        this.local=place;
+    }
 }
